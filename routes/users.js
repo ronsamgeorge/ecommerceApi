bcdc9876ecd6cf52314bb8c97b41386b3697f451
Ronald George <ronaldsamgeorge@gmail.com>
2023-01-23 23:27:42 +0530

    Add middleware for token authenication

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,29 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
+
+// updating user details
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true } // to return the updated user info
+    );
+    res.status(201).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,47 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+
+  // no authentication token, return error
+  if (!authHeader) {
+    return res.status(401).json("You are not authenticated");
+  }
+
+  const token = authHeader.split(" ")[1];
+  // verify auth tokem
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      res.status(403).json("Token is not valid");
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    // check if token is of the user whose detail will be updated
+    // or if they are the admnin
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next(); //
+    } else {
+      res.status(403).json("Unauthorized to carry out the action");
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    // check if token is of the user whose detail will be updated
+    // or if they are the admnin
+    if (req.user.isAdmin) {
+      next(); //
+    } else {
+      res.status(403).json("Not an admin Unauthorized to carry out the action");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
