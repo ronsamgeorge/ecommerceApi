73340d8f1e5d8d1dfb7a8866c7641d2d0756c669
Ronald George <ronaldsamgeorge@gmail.com>
2023-01-24 16:53:10 +0530

    Add Product routing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/products");
 
 const dbUrl = process.env.MONGO_URL;
 const port = process.env.PORT || 8080;
@@ -22,6 +23,7 @@ app.use(express.json());
 // api endpoint routing
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 // the server will listen on the specified PORT for incoming traffic, if no port given in env, use 8080
 app.listen(port, () => {
--- routes/products.js
+++ routes/products.js
@@ -1,3 +1,82 @@
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
 const router = require("express").Router();
+
+// create Product
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// updating user details
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true } // to return the updated user info
+    );
+    res.status(201).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete a user
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get User info
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get all Users
+router.get("/", async (req, res) => {
+  const queryNew = req.query.new;
+  const queryCategory = req.query.category;
+
+  try {
+    let productList;
+
+    if (queryNew) {
+      productList = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (queryCategory) {
+      productList = await Product.find({
+        category: { $in: [queryCategory] },
+      });
+    } else {
+      productList = await Product.find();
+    }
+
+    res.status(200).json(productList);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
