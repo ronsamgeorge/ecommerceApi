e936be9a2506f14e3d9532b56ef19452d0c1306f
Ronald George <ronaldsamgeorge@gmail.com>
2023-01-25 17:37:03 +0530

    Integrate Payment with Stripe

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/products");
 const cartRoute = require("./routes/carts");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 const dbUrl = process.env.MONGO_URL;
 const port = process.env.PORT || 8080;
@@ -22,12 +24,15 @@ mongoose
 // to parse req json bodies
 app.use(express.json());
 
+app.use(cors());
+
 // api endpoint routing
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 // the server will listen on the specified PORT for incoming traffic, if no port given in env, use 8080
 app.listen(port, () => {
---
+++ routes/stripe.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+router.get("/payment", (reqm, res) => {
+  res.status(200).json("it is working");
+});
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "inr",
+    },
+    (stripeError, stripeRes) => {
+      if (stripeError) {
+        res.status(501).json(stripeError);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+module.exports = router;
